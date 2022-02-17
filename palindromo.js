2794df04311dd47dca9c7c1bbab4c430834b1291
gabriel-palhares <gabriel.edu.p@gmail.com>
2022-02-17 10:41:35 -0300

    adding the challenges solution code

---
+++ palindromo.js
@@ -0,0 +1,22 @@
+// primeira forma de solução
+function verificaPalindromo(string) {
+	if (!string) return;
+	console.log(string === string.split('').reverse().join(''));
+}
+verificaPalindromo('cat');
+
+// segunda forma de solução
+
+function varificaPalindromo2(string) {
+	if (!string) return;
+	if (!string.length) return;
+
+	for (var i = 0; i < string.length / 2; i++) {
+		if (string[i] !== string[string.length - 1 - i]) {
+			return console.log(false);
+		}
+	}
+	return console.log(true);
+}
+
+varificaPalindromo2('asa');
